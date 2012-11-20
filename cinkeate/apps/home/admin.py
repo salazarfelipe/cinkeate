7dd9369e1c445b98f363838ed1b2b1b39e304c03
Steven Pineda Cortés <steven2308@hotmail.es>
2012-11-20 16:55:59 -0500

    Modelos definidos
    Aún presenta problemas por el path para subir los archivos, y falta
    definir q variables van a ser obligatorias y cuales no.

--- cinkeate/apps/home/admin.py
+++ cinkeate/apps/home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from cinkeate.apps.home.models import Materia,Programa,Usuario,Profesor, Parcial, Hoja_Parcial
+from cinkeate.apps.home.models import Materia,Programa,Usuario,Profesor, Parcial, Hoja_Parcial, Actividad, Actividad_Materia, Actividad_Parcial
 
 admin.site.register(Materia)
 admin.site.register(Programa)
@@ -7,3 +7,6 @@ admin.site.register(Profesor)
 admin.site.register(Usuario)
 admin.site.register(Parcial)
 admin.site.register(Hoja_Parcial)
+admin.site.register(Actividad)
+admin.site.register(Actividad_Materia)
+admin.site.register(Actividad_Parcial)
