3cf16bd120ff226b149d12bbed20a4f24e6b1cca
elpipesalazar <elpipesalazar@gmail.com>
2013-03-09 13:24:24 -0500

    Se añade bootstrap y se define platilla base para la interfaz

--- cinkeate/apps/home/urls.py
+++ cinkeate/apps/home/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('cinkeate.apps.home.views',
     url(r'^$', 'index_view', name='vistaPrincipal'),
     url(r'^login/$', 'login_view', name='vistaLogin'),
+    url(r'^logout/$', 'logout_view', name='vistaLogout'),
     url(r'^home/$', 'home_view', name='vistaHome'),
 )
--- cinkeate/apps/home/views.py
+++ cinkeate/apps/home/views.py
@@ -4,10 +4,24 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import login, authenticate, logout
 from cinkeate.apps.home.models import Materia
 
-
+# Controla el index o pagina inicial
+# si no se ha iniciado sesion, renderuza la vista de index
+# Si ya hay una sesion activa, determina que tipo de usuario es
+# y lo envia a su respectiva vista
 def index_view(request):
-	return render_to_response('index.html', locals(), context_instance = RequestContext(request) )
+	if request.user.is_authenticated():
+		if request.user.is_staff:
+			return HttpResponseRedirect('/admin')
+		else:
+			return HttpResponseRedirect('/home')
+	else:
+		return render_to_response('index.html', locals(), context_instance = RequestContext(request) )
 
+
+
+# controla el login de usuario
+# Si es usuario administrador lo envia a la vista de admin
+# si es usuario convencional lo envia al home (perfil de usuario)
 def login_view(request):
 	try:
 		username = request.POST['username']
@@ -25,5 +39,18 @@ def login_view(request):
 	loginFailed = True
 	return render_to_response('index.html', locals(), context_instance = RequestContext(request) )
 
+
+
+# Controlador del Cierre de Sesion
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+
+#Controlador de vista de home *Vista de Perfil proximamente
 def home_view(request):
-	return render_to_response('home.html',locals(), context_instance = RequestContext(request))
+	if request.user.is_authenticated():
+		return render_to_response('home.html',locals(), context_instance = RequestContext(request))
+	else:
+		return HttpResponseRedirect('/')
