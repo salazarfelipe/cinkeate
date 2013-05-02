9ff02c92cb8f5eb1bea79be990794b6217bd5465
Hugo Andres Gomez <sureno755@gmail.com>
2013-05-02 15:56:07 -0500

    cambios para busqueda

--- cinkeate/apps/home/urls.py
+++ cinkeate/apps/home/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('cinkeate.apps.home.views',
     url(r'^$', 'index_view', name='vistaPrincipal'),
+    url(r'^vistaBusqueda/$', 'search_view', name='vistaBusqueda'),
 )
