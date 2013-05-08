5585f1a331634d2fbfffc3f9817d42c2cfe0c990
Hugo Andres Gomez <sureno755@gmail.com>
2013-05-08 16:14:51 -0500

    Vista Búsqueda y actualización plantila base

--- cinkeate/apps/home/views.py
+++ cinkeate/apps/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.contrib.auth import login, authenticate, logout
-from cinkeate.apps.home.models import Materia, Usuario, Programa
+from cinkeate.apps.home.models import Materia, Usuario, Programa, Profesor, Parcial
 from django.contrib.auth.models import User
 
 # Controla el index o pagina inicial
@@ -99,6 +99,19 @@ def profile_view(request):
 		current_user = request.user
 		extended_user= Usuario.objects.get(user=current_user)
 		return render_to_response('profile.html', locals(),context_instance = RequestContext(request))	
+	else:
+		return HttpResponseRedirect('/')
+
+#Busqueda
+def search_view(request):
+	if request.user.is_authenticated():
+		if request.user.is_staff:
+			return HttpResponseRedirect('/admin')
+		else:
+			materias=Materia.objects.all()
+			profesores=Profesor.objects.all()
+			notas=Parcial.objects.all()
+			return render_to_response('searchview.html', locals(),context_instance = RequestContext(request))	
 	else:
 		return HttpResponseRedirect('/')
 
