48cef59e987d7156929eb20e36bbc4061658b811
Felipe Salazar <elpipesalazar@gmail.com>
2014-06-10 11:16:42 -0500

    change in settings -- database information

--- cinkeate/settings.py
+++ cinkeate/settings.py
@@ -16,10 +16,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'cinkeate',                      # Or path to database file if using sqlite3.
-        'USER': 'dhfs',                      # Not used with sqlite3.
-        'PASSWORD': 'muyfacildeadivinar',                  # Not used with sqlite3.
-        'HOST': '198.61.224.120',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
